5540d8a366c4d716135bc0ff02d77d62b9d8c5d9
William-Yulio <william.yulio331@gmail.com>
2022-04-28 01:32:29 +0700

    Initial Commit

---
+++ do.it App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  do.it App
+//
+//  Created by William Yulio on 28/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
